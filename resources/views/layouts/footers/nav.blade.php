1bca0d2fa13ab1dcd286e9e4fb62276832582d75
adrjob <adrian@salehabdul.com>
2022-08-12 16:36:16 +0300

    layout fix

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,7 +1,7 @@
 <div class="row align-items-center justify-content-lg-between">
     <div class="col-xl-12">
         <div class="copyright text-center text-lg-center text-muted" style="color: black !important;">
-            &copy; {{ now()->year }} <a href="https://www.vanuatusky.com" class="font-weight-bold ml-1" target="_blank" style="color: black !important;">Vanuatu Sky</a>
+            &copy; {{ now()->year }} <a href="{{ route('home') }}" class="font-weight-bold ml-1" style="color: black !important;">Vanuatu Sky</a>
         </div>
     </div>
     </div>
